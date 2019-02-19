3bd7fe503048ded63034a1ea3f551089aa3a32dc
francesc <elskater98@gmail.com>
2019-02-19 10:23:30 +0100

    Seacrh_relevant information

--- packtpub_download.py
+++ packtpub_download.py
@@ -7,14 +7,23 @@ import bs4
 class Client_Web(object):
     """docstring fo Client_Web."""
     def __init__(self):
-        super(Client_Web, self).__init__()
+        pass
 
     def download_html_page(self):
         html_file = urlopen("https://www.packtpub.com//packt/offers//free-learning")
-        print(html_file)
+        html_file.read()
         html_file.close()
+        return html_file
+
+    def search_relevant_information(self,html_file):
+        tree = bs4.BeautifulSoup(html_file)
+        print ( tree. prettify() )
+
+    def run(self):
+        html = self.download_html_page()
+        self.search_relevant_information(html)
 
 
 if __name__=="__main__":
     client= Client_Web()
-    client.download_html_page()
+    client.run()
