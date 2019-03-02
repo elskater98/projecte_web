29a436e1c59c82efc4be1a15c5e9aa9096e0bfa7
Francesc <elskater98@gmail.com>
2019-03-02 15:29:01 +0100

    S'ha optimitzat la busqueda de dades i s'ha estructurat correctament

--- bangood_scraping.py
+++ bangood_scraping.py
@@ -13,26 +13,35 @@ class Client_Web(object):
 
     def process_all_products(self,html):
         tree = bs4.BeautifulSoup(html,features="lxml")
-        products = tree.find_all("span","title")
+        self.print_product_information(self.get_titles(tree),self.get_prices(tree))
+
+
+    def get_titles(self,tree):
+        list_product = []
+        products = tree.find_all("span", "title")
 
         for product in products:
-            self.process_product(product.find("a")["href"])
+            list_product.append(product.find("a").text)
 
+        return list_product
 
-    def process_product(self,product):
-        html = self.do_request(product)
-        tree = bs4.BeautifulSoup(html, features="lxml")
+    def get_prices(self,tree):
+        list_prices = []
+        price_items = tree.find_all("div", "priceitem")
 
-        title = tree.find("div","title_hd")
-        print("Name of product:",title.find("h2")["data-oldtext"])
+        for price in price_items:
+            list_prices.append((price.find("span","price").text,price.find("span","price_old").text))
 
-        current_price = tree.find("div","item_now_price")
-        print("Current price:",current_price.text)
+        return list_prices
 
-        #old_price = tree.find("div","vipbox")
-        #old_price=old_price.find("span","vip_old_price")
-        #print(old_price)
-        print("\n")
+    def print_product_information(self, list_product, list_prices):
+
+        for i in range(len(list_prices)):
+            print("TITLE: ", list_product[i],
+                  "\nCURRENT PRICE: ", list_prices[i][0],
+                  "\nOLD PRICE: ",list_prices[i][1])
+            print("\n")
+
 
     def run(self):
         data = self.do_request()
